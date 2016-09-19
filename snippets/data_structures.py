93d82f939c71a5929abf129b5d69329f74bdf25d
Allen Wang <allenwang28@yahoo.com>
2016-09-19 00:27:30 -0500

    Added a data structures notebook

--- snippets/data_structures.py
+++ snippets/data_structures.py
@@ -15,12 +15,38 @@ def list_ex():
     # Lists also support concatenation
     print(my_non_empty_list + [5, 6, 11, 13])
 
+    # You can get the length of a list
+    print(len(my_non_empty_list))
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    print(letters)
+    # You can replace some values by slices
+    letters[2:5] = ['C', 'D', 'E']
+    print(letters)
+
+    # Now remove them
+    letters[2:5] = []
+    print(letters)
+
+    # Searching for values in a list
+    search_list = ['harambe', 'was', 'an', 'inside', 'job']
+
+    # Prints number of 'was' in the list
+    print(search_list.count('was'))
+
+    # Prints the index of 'harambe'
+    print(search_list.index('harambe'))
+
+    # Errors if not in list
+#    print(search_list.index('justice'))
+
 
 def dict_ex():
     # Ways to initialize a dictionary
     my_dict = {}
 
 def tuple_ex():
+    # Tuples are immutable lists. It can't be changed in any way once it's created
     # Ways to initialize a tuple
     my_tuple = ()
 
